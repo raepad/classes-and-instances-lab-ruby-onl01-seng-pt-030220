6be28104c2f02b22cb13b47d1445d8b8e1ae6ed5
Rachel Padworski <rachelpadworski@gmail.com>
2020-03-15 16:46:42 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,18 +2,21 @@ class Dog
   fido = Dog.new 
   
   snoopy = Dog.new 
-  snoopy
+  
   lassie = Dog.new 
-  lassie
+  
 end 
-
+fido
+snoopy 
+lassie 
 
 
 class Person
-  
+  adele_goldberg = Person.new
+  alan_kay = Person.new 
 end 
 
-adele_goldberg = Person.new 
+ 
 adele_goldberg
 alan_kay = Person.new 
 alan_kay
